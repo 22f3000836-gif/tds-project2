554adc42adf3a93e6278fd9bdbb1d51f36a79f84
22f3000836-gif <22f3000836@ds.study.iitm.ac.in>
2025-11-28 17:58:24 +0530

    Import functions from various modules in __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,1 +1,19 @@
+# tools/__init__.py
 
+# Assuming your file is named web_scraper.py
+from .web_scraper import get_rendered_html
+
+# Assuming your file is named download_file.py
+from .download_file import download_file
+
+# Check if your file is named 'send_request.py' or 'post_request.py'
+# If the file is 'send_request.py', keep the line below:
+from .send_request import post_request 
+# IF the file is named 'post_request.py', change it to: from .post_request import post_request
+
+# Based on your previous error, your file seems to be named 'run_code.py'
+from .run_code import run_code
+# If the file is actually named 'code_generate_and_run.py', use: from .code_generate_and_run import run_code
+
+# Assuming your file is named add_dependencies.py
+from .add_dependencies import add_dependencies
